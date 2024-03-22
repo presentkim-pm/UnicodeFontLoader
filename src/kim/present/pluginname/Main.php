9894e09415fc2219d4c236d417d1f26842ab4971
PresentKim(김현재) <debe3721@gmail.com>
2024-03-22 11:16:54 +0900

    Initial commit

---
+++ src/kim/present/pluginname/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author       PresentKim (debe3721@gmail.com)
+ * @link         https://github.com/PresentKim
+ * @license      https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpUnused
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\pluginname;
+
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase{
+	protected function onLoad() : void{
+		$this->getLogger()->info("Template plugin loaded!");
+	}
+
+	protected function onEnable() : void{
+		$this->getLogger()->info("Template plugin enabled!");
+	}
+
+	protected function onDisable() : void{
+		$this->getLogger()->info("Template plugin disabled!");
+	}
+}
