3826f29d6ef72a5677eb8daebf7ce0eb4e0c4cee
PresentKim <debe3721@gmail.com>
2024-03-22 11:19:14 +0900

    :tada: Init UnicodeFontLoader plugin

--- src/kim/present/unicodefontloader/Main.php
+++ src/kim/present/unicodefontloader/Main.php
@@ -25,20 +25,11 @@
 
 declare(strict_types=1);
 
-namespace kim\present\pluginname;
+namespace kim\present\unicodefontloader;
 
 use pocketmine\plugin\PluginBase;
 
 final class Main extends PluginBase{
-	protected function onLoad() : void{
-		$this->getLogger()->info("Template plugin loaded!");
-	}
-
 	protected function onEnable() : void{
-		$this->getLogger()->info("Template plugin enabled!");
-	}
-
-	protected function onDisable() : void{
-		$this->getLogger()->info("Template plugin disabled!");
 	}
 }
